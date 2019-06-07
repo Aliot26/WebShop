e933015345bd8bca09a2da6550265e57027ec99f
Kat <kat.m.borowska@gmail.com>
2019-06-07 07:29:07 +0200

    add new category "laptop"

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -37,15 +37,18 @@ public class Initializer implements ServletContextListener {
         //setting up a new product category
         ProductCategory allCategories = new ProductCategory("All categories", "", "");
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "A personal computer, you need it to code");
         ProductCategory ebook = new ProductCategory("Ebook", "Portable", "blablabla");
         productCategoryDataStore.add(allCategories);
         productCategoryDataStore.add(tablet);
         productCategoryDataStore.add(ebook);
+        productCategoryDataStore.add(laptop);
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("Kindle", 50, "USD", "We love Kindle :)", ebook, amazon));
+        productDataStore.add(new Product("Kindle Paperwhite", 50, "USD", "We love Kindle :)", ebook, amazon));
+        productDataStore.add(new Product("Lenovo Yoga 460", 429, "USD", "Convertible business premium laptop", laptop, lenovo));
     }
 }
