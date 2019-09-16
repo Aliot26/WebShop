4d98fee383618f481f452cdf904e3df018abfb24
Aliot26 <olgakochanowa@gmail.com>
2019-09-16 19:33:05 +0200

    refactoring

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoMem.java
@@ -51,7 +51,12 @@ public class ProductDaoMem implements ProductDao {
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        return data.stream().filter(t -> t.getSupplier().equals(supplier)).collect(Collectors.toList());
+        return data
+                .stream()
+                .filter(t -> t.getSupplier()
+                        .equals(supplier))
+                .collect(Collectors
+                        .toList());
     }
 
     @Override
